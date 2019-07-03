72b707d5e9fdb61b8b51efeecb5492ab5ac6f717
Matt Magoffin <matt@solarnetwork.net>
2019-07-03 17:50:27 +1200

    Add CLI helper methods for presenting list of items.

--- common-cli/src/main/java/net/solarnetwork/esi/cli/BaseShellSupport.java
+++ common-cli/src/main/java/net/solarnetwork/esi/cli/BaseShellSupport.java
@@ -18,8 +18,16 @@
 package net.solarnetwork.esi.cli;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.function.BiConsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyAccessor;
+import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
@@ -35,6 +43,9 @@ public abstract class BaseShellSupport extends BaseMessageSourceSupport {
 
   /** The shell helper. */
   protected final SshShellHelper shell;
+
+  /** A class-level logger. */
+  protected final Logger log = LoggerFactory.getLogger(getClass());
 
   /**
    * Constructor.
@@ -143,4 +154,115 @@ public abstract class BaseShellSupport extends BaseMessageSourceSupport {
     }
   }
 
+  /**
+   * Show a numbered list of objects, returning a list of one of their property values.
+   * 
+   * <p>
+   * This method renders a numbered list of items based on property values from a list of objects.
+   * This can be used to render a list to pick an item from. The method returns a list of "key"
+   * values from each object. Typically these would be a unique value, such as a primary key of an
+   * entity, that can be used to uniquely identify each object.
+   * </p>
+   * 
+   * @param <K>
+   *        the item property value return type
+   * @param <V>
+   *        the item type
+   * @param list
+   *        the list of objects to show
+   * @param itemCode
+   *        the message key to use for each list item; will be passed a parameter list including the
+   *        1-based list index and any {@code propName} values
+   * @param keyPropName
+   *        the property name to return the values of
+   * @param propNames
+   *        property names to include as list item message parameters
+   * @param itemCallback
+   *        an optional callback to invoke for each list item, after its message has been printed to
+   *        the shell
+   * @return the list of {@code keyPropName} property values
+   */
+  protected <K, V> List<K> showNumberedObjectList(Iterable<V> list, String itemCode,
+      String keyPropName, String[] propNames, BiConsumer<Integer, V> itemCallback) {
+    int idx = 0;
+    List<K> ids = new ArrayList<>();
+    for (V info : list) {
+      idx++;
+      PropertyAccessor bean = PropertyAccessorFactory.forBeanPropertyAccess(info);
+
+      @SuppressWarnings("unchecked")
+      K key = (K) bean.getPropertyValue(keyPropName);
+      ids.add(key);
+
+      Object[] params = new Object[(propNames != null ? propNames.length : 0) + 1];
+      params[0] = idx;
+      if (propNames != null) {
+        for (int i = 0; i < propNames.length; i++) {
+          Object val = null;
+          try {
+            val = bean.getPropertyValue(propNames[i]);
+          } catch (BeansException e) {
+            log.warn("Error reading bean property [{}] on [{}]: {}", propNames[i], info,
+                e.toString());
+          }
+          params[i + 1] = val;
+        }
+      }
+      String msg = messageSource.getMessage(itemCode, params, Locale.getDefault());
+      if (itemCallback != null) {
+        msg = ShellUtils.getBold(msg);
+      }
+      shell.print(msg);
+      if (itemCallback != null) {
+        itemCallback.accept(idx, info);
+      }
+    }
+    return ids;
+  }
+
+  /**
+   * Show a list of items and prompt to pick one of the, returning the chosen item's key.
+   * 
+   * @param <K>
+   *        the item property value return type
+   * @param <V>
+   *        the item type
+   * @param list
+   *        the list of items to choose from
+   * @param codePrefix
+   *        a message code prefix to prepend to all prompt messages
+   * @param keyPropName
+   *        the item property name that provides the key value to use
+   * @param propNames
+   *        an optional additional list of item property names to pass to the item list message,
+   *        after the list item's 1-based index
+   * @param itemCallback
+   *        an optional callback to invoke after each list item is printed
+   * @return the key of the chosen item
+   * @see #showNumberedObjectList(Iterable, String, String, String[], BiConsumer)
+   */
+  protected <K, V> K promptForNumberedObjectListItem(Iterable<V> list, String codePrefix,
+      String keyPropName, String[] propNames, BiConsumer<Integer, V> itemCallback) {
+    List<K> ids = showNumberedObjectList(list, codePrefix + ".item", keyPropName, propNames,
+        itemCallback);
+    if (ids.isEmpty()) {
+      shell.printWarning(messageSource.getMessage(codePrefix + ".none", null, Locale.getDefault()));
+      return null;
+    }
+
+    while (true) {
+      String ans = shell
+          .read(messageSource.getMessage(codePrefix + ".pick", null, Locale.getDefault()));
+      try {
+        int index = Integer.parseInt(ans) - 1;
+        return ids.get(index);
+      } catch (NumberFormatException e) {
+        shell.printError(
+            messageSource.getMessage("answer.error.enterNumber", null, Locale.getDefault()));
+      } catch (ArrayIndexOutOfBoundsException e) {
+        shell.printError(messageSource.getMessage("answer.error.numOutOfRange",
+            new Object[] { 1, ids.size() }, Locale.getDefault()));
+      }
+    }
+  }
 }
